0953b996f46a8de0aae3995bdb9cfd43f7adc166
dipak kawale <iamdkawle@gmail.com>
2022-04-04 20:20:22 +0530

    checking1

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
-self.addEventListener("install", async (event) => {
-  console.log("install event");
-});
-
-self.addEventListener("fetch", async (event) => {
-  console.log("fetch event");
-});
 const cacheName = "pwa-conf-v1";
-const staticAssets = ["./", "./index.html", "./js/app.js", "./css/styles.css"];
+const staticAssets = [
+  "./",
+  "./index.html",
+  "./about.html",
+  "./car-details.html",
+  "./cars.html",
+  "./dashboard.html",
+  "./login.html",
+  "./services.html",
+  "./app.js",
+  "./style.css",
+];
+
 self.addEventListener("install", async (event) => {
   const cache = await caches.open(cacheName);
   await cache.addAll(staticAssets);
 });
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || fetch(req);
+}
